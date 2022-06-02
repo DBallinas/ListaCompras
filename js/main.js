7f93a30f9ed55b4b0ac31a619dc0b83666c1ec5a
davidvazquez <davidballinas65@gmail.com>
2022-06-02 15:37:20 -0500

    Se agregaron funciones a la lista

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let contador = 0;
+
 let element = document.getElementById("totalprecio")
 element.innerHTML="Total en precio";
 
@@ -5,50 +7,82 @@ let txtNombre = document.getElementById("Name");
 // txtNombre.value="Leche Semidescremada";
 // console.log(txtNombre.value);
 let txtNUmber = document.getElementById("Number");
-
-
 // let campos = document.getElementsByClassName("campo");
 // campos[0].value= "leche descremada deslactosada light-agua";
 // console.log(campos[0].value);
 // console.log(campos);
-
 // for (let i = 0; i < campos.length; i++) {
 //     campos[i].style.border="red thin solid";
 // }//for
-
 // let spans = document.getElementsByTagName("span");
 // for (let i = 0; i < spans.length; i++) {
 //    console.log(spans[i].textContent);
 // }//for i
 
-let tabla = document.getElementById("tablaListaCompras");
+let tabla = document.getElementById("tableListaCompras");
+
 let cuerpoTabla = tabla.getElementsByTagName("tbody");
 
-// cuerpoTabla[0].innerHTML = `
-// <tr> 
-// <th scope="row">1</th>
-// <td>Leche descremada</td>
-// <td>1</td>
-// <td>$ 23</td>
-// </tr> `;
+function validarCantidad(){
+    if(txtNumber.value.length==0) {
+        return false;
+    }// if
+     if (isNaN(txtNumber.value)){
+        return false;
+     }//if
 
+     if (parseFloat(txtNumber.value)<=0) {
+        return false;
+     }//if
+     return true;
+}// validarCantidad
 let agregar = document.getElementById("btnagregar");
+
 agregar.addEventListener("click", (event)=>{
-    let precio = Math.random() * 50;
-    let tmp =
-    `<tr> 
-<th scope="row">1</th>
-<td>${txtNombre.value}</td>
-<td>${txtNumber.value}</td>
-<td>$ ${precio}</td>
-</tr> `;
-// console.log(txtNombre.ariaValueMax, txtNUmber.value);
+    contador++;
+    document.getElementById("contadorProductos").innerHTML=contador;
+    let precio = (Math.floor( (Math.random() * 50)*100))/100;
+    let tmp = `<tr> 
+    <th scope="row">${contador}</th>
+    <td>${txtNombre.value}</td>
+    <td>${txtNumber.value}</td>
+    <td>$ ${precio.toFixed(2)}</td>
+<   /tr> `;
     console.log(tmp);
-    cuerpoTabla[0].innerHTML -= tmp;
+    cuerpoTabla[0].innerHTML += tmp;
     txtNumber.value="";
     txtNombre.values="";
     txtNombre.focus();
 }
 ); 
 
-// console.log(agregar);
+let agregar = document.getElementById("btnagregar");
+agregar.addEventListener ("click", (event) =>{
+    event.preventDefault();
+    if ((! validarNombre()) || (! validarCantidad())){
+        let lista ="";
+        if(!validarNombre()){
+            txtNombre.style.border="red thin solid";
+            lista+="<li>Se debe escribir un nombre valido</li>";
+        }//
+        if(!validarCantidad()){
+            txtNumber.style.border="red thin solid";
+            lista+="<li>Se debe escribir una cantidad  valido</li>";
+        }//
+        document.getElementById("alertValidacionesTexto").innerHTML=`
+        Los campos deben ser llenados correctamente.
+        <ul>${lista}</ul>
+        `;
+        document.getElementById("alertValidaciones").style.display="block";
+        setTimeout(function(){
+            document.getElementById("alertValidaciones").style.display="none";
+},
+5000
+);
+return false;
+}//if
+txtNumber.style.border="";
+txtNombre.style.border="";
+document.getElementById("alertValidaciones").style.display="none";
+contador++;
+document.getElementById("contadorProductos").innerHTML=contador;
